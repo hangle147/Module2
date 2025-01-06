9a4971d19a95654ae954304aa59aa48ac425a266
hangle147 <nguyenxhuong1105@gmail.com>
2025-01-06 19:33:53 +0700

    baitap

--- src/module2/src/Loops_in_Java/Display.java
+++ src/module2/src/Loops_in_Java/Display.java
@@ -13,68 +13,80 @@ public class Display {
         System.out.print("Enter your choice: ");
         int choice = sc.nextInt();
 
-        switch(choice) {
-            case 1:
-                System.out.println("Draw the rectangle");
-                System.out.println("* * * * * *");
-                System.out.println("* * * * * *");
-                System.out.println("* * * * * *");
-            case 2:
-                System.out.println("The corner is square at: ");
-                System.out.println("1) top-left");
-                System.out.println("2) top-right");
-                System.out.println("3) bottom-left");
-                System.out.println("4) bottom-right");
-                System.out.print("Enter your choice: ");
-                int choiceCorner = sc.nextInt();
+        while (true) {
+            switch(choice) {
+                case 1:
+                    System.out.println("Draw the rectangle");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                    System.out.println("* * * * * *");
+                case 2:
+                    System.out.println("The corner is square at: ");
+                    System.out.println("1) top-left");
+                    System.out.println("2) top-right");
+                    System.out.println("3) bottom-left");
+                    System.out.println("4) bottom-right");
+                    System.out.println("5) return");
+                    System.out.print("Enter your choice: ");
+                    int choiceCorner = sc.nextInt();
 
-                switch(choiceCorner) {
-                    case 1:
-                        System.out.println("Draw the triangle at top-left");
-                        for (int i = 5; i > 0; i--) {
-                            for (int j = 0; j < i; j++) {
-                                System.out.print("*");
-                            }
-                            System.out.print("\n");
-                        }
-                        break;
-                    case 2:
-                        System.out.println("Draw the triangle at top-right");
-                        for (int i = 5; i > 0; i--) {
-                            for (int j = 1; j < 5 - i; j++) {
-                                System.out.println(" ");
-                            }
-                            for (int k = 0; k <= i; k++) {
-                                System.out.println("*");
-                            }
-                        }
-                        break;
-                    case 3:
-                        System.out.println("Draw the triangle at bottom-left");
-                        for (int i = 0; i < 5; i++) {
-                            for (int j = 0; j <= i; j++) {
-                                System.out.print("*");
-                            }
-                            System.out.print("\n");
-                        }
-                        break;
-                    case 4:
-                        System.out.println("Draw the triangle at bottom-right");
-                        for (int i = 5; i > 0; i--) {
-                            for (int j = 1; j < i; j--) {
-                                System.out.print(" ");
-                            }
-                            for (int k = 0; k <= 5 - i; k++) {
-                                System.out.print("*");
-                            }
-                        }
-                        break;
-                }
+                    triangle(choiceCorner);
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+        }
+    }
 
-            case 0:
-                System.exit(0);
-            default:
-                System.out.println("No choice!");
+    private static void triangle(int choiceCorner) {
+        while (true) {
+            switch(choiceCorner) {
+                case 1:
+                    System.out.println("Draw the triangle at top-left");
+                    for (int i = 5; i > 0; i--) {
+                        for (int j = 0; j < i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Draw the triangle at top-right");
+                    for (int i = 5; i > 0; i--) {
+                        for (int j = 1; j < 5 - i; j++) {
+                            System.out.println(" ");
+                        }
+                        for (int k = 0; k <= i; k++) {
+                            System.out.println("*");
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("Draw the triangle at bottom-left");
+                    for (int i = 0; i < 5; i++) {
+                        for (int j = 0; j <= i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Draw the triangle at bottom-right");
+                    for (int i = 5; i > 0; i--) {
+                        for (int j = 1; j < i; j--) {
+                            System.out.print(" ");
+                        }
+                        for (int k = 0; k <= 5 - i; k++) {
+                            System.out.print("*");
+                        }
+                    }
+                    break;
+                case 5:
+                    return;
+                default:
+                    System.out.println("No choice!");
+            }
         }
     }
 }
